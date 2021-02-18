f5ad935295c295d46c6fd278f31eb49276c19f5d
KevinPozuelos <2723810200106@ingenieria.usac.edu.gt>
2021-02-18 12:42:02 -0600

    Menu principal del proyecto

---
+++ Menu.py
@@ -0,0 +1,34 @@
+def menu():
+    print("Practica 1")
+    print("-----------------")
+    print("1.- Cargar archivo de entrada")
+    print("2.- Desplegar listas ordenadas")
+    print("3.- Desplegar búsquedas")
+    print("4.- Desplegar todas ")
+    print("5.- Desplegar todas a archivo")
+    print("6.- Salir")
+    opcion = input("Eliga su opcion: ")
+    return opcion
+
+def menuP():
+    while True:
+        opcion = menu()
+        #if opcion == '1':
+
+        #if opcion == '2':
+
+        #if opcion == '3':
+
+        #if opcion == '4':
+
+
+        #if opcion == '5':
+
+
+        if opcion == '6':
+            print("KEVIN RAUL POZUELOS")
+            print("CARNE: 201800992")
+            print("Introduccion a la programacion y computacion 2 Seccion: ""A""")
+            print("Ingenieria en Ciencias y Sistemas")
+            print("4to Semestre")
+            break
