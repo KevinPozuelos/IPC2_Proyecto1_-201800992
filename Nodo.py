b9f34e6e35f06e573410691d47884d130c542eae
KevinPozuelos <2723810200106@ingenieria.usac.edu.gt>
2021-02-20 17:26:03 -0600

    Nodo de lista circular doble destina a guardar los datos de la matriz, como lo es: nombre, n, valores numericos y coordenadas. Estas ultimas seran almacenadas en una lista simple enlazada.

---
+++ Nodo.py
@@ -0,0 +1,9 @@
+from Dato import *
+class Nodo:
+    def __init__(self, nombre, n, m, dato):
+        self.nombre = nombre
+        self.n = n
+        self.m = m
+        self.dato = dato
+        self.next = None
+        self.before = None
